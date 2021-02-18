4328c84b52265cb70288f504d281587e217652af
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-02-18 18:12:13 +0300

    Question 07 import is done.

--- asset/js/07(Modules_Export).js
+++ asset/js/07(Modules_Export).js
@@ -7,21 +7,23 @@ export const myVar = "Export Var By Adding Export Keyword In the Front!!";
 function exp1() {
     return "Export as a list!!!";
 }
+
 //export this function at last line
 function exp2() {
     return "Export as an Alias!!!";
 }
+
 //export this function at last line
 function exp3() {
     return "Import as an Alias!!!";
 }
+
+
 //2. List export(comma separated list )
+
 export {exp1,exp2,exp3}
-
-
 //3. Export exp2 with AliasName ==> exp2_Alias
 export {exp2 as exp2_Alias };
 
-
 //4. Export exp3
-export {exp3}
+export {exp3 as exp3_Alias}
