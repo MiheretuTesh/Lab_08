c3e6f776cc447ac804483fd29f8dad4f840c73e5
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-02-18 16:09:40 +0300

    Question 05(OOP@) is done.

--- asset/js/05(OOP2).js
+++ asset/js/05(OOP2).js
@@ -5,12 +5,23 @@ const PersonObj = {
         return `Hi , ${this.firstName} ${this.lastName}`;
     }
 }
+const person = Object.create(PersonObj);
+person.firstName = "Person"
+person.lastName = "person"
+console.log(person.greeting())
 
 //2. Create Theon Object using Person Template
-
+const Theon = {
+    greeting() {
+        return `Hi , ${this.firstName} ${this.lastName}`;
+    }
+}
+let theon = Object.create(Theon);
+theon.firstName = "Theon";
+theon.lastName = "Greyjoy"
 
 //3. Add firstName[Theon] and lastName[Greyjoy] 
 
 
 //4. Display the greeting on Theon Object [remove the string when you have the object]
-Obj_C2.innerHTML = "Theon.greeting()";
+Obj_C2.innerHTML = theon.greeting();
